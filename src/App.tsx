0754f29b6d9b1d6c96eff056e2142e7cd33ce3b6
JessicaaSun <sunjessica05@gmail.com>
2024-03-09 14:30:37 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Todos } from "./components/Todos"
+
+
+function App() {
+  return (
+    <div className="App h-screen flex justify-center items-center bg-gray-100">
+      <Todos />
+    </div>
+  )
+}
+
+export default App
